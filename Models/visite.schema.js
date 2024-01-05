bf438b47885a5f759c80b97d237cac5ff5dc4ef6
Raouf <raoufslv09@gmail.com>
2024-01-05 09:52:14 +0100

    adding ordonnance schema

--- Models/visite.schema.js
+++ Models/visite.schema.js
@@ -1,32 +1,12 @@
 import mongoose from "mongoose";
 
-// const Ordonnance = new mongoose.Schema({
-//   visiteId: {
-//     type: mongoose.Types.ObjectId,
-//      ref: "Visite",
-//   },
-//   Medicaments: [
-//     {
-//       name: {
-//         type: String,
-//       },
-//       dosage: {
-//         type: Number,
-//       },
-//       duree: {
-//         type: Number,
-//       },
-//     },
-//   ],
-// });
-
 const Visite = mongoose.model(
   "visites",
   new mongoose.Schema({
     patientId: {
       type: mongoose.Types.ObjectId,
       ref: "Patient",
-      required:true,
+      required: true,
     },
     Numero_visite: {
       type: Number,
